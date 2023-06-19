1bd123bd09c45f4d61434338d4605c318d7f36c4
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-06-19 12:07:23 +0200

    Lesson about triple-dimensional arrays.

--- src/main/java/dev/lpa/Main.java
+++ src/main/java/dev/lpa/Main.java
@@ -31,5 +31,30 @@ public class Main {
 //        }
 
         System.out.println(Arrays.deepToString(array2));
+
+        array2[1] = new int[] {10, 20, 30};
+        System.out.println(Arrays.deepToString(array2));
+
+        Object[] anyArray = new Object[3];
+        System.out.println(Arrays.toString(anyArray));
+
+        anyArray[0] = new String[] {"a", "b", "c"};
+        System.out.println(Arrays.deepToString(anyArray));
+
+        anyArray[1] = new String[][]{
+                {"1", "2"},
+                {"3", "4", "5"},
+                {"6", "7", "8", "9"}
+        };
+        System.out.println(Arrays.deepToString(anyArray));
+
+        anyArray[2] = new int[2][2][2];
+        System.out.println(Arrays.deepToString(anyArray));
+
+        for (Object element : anyArray) {
+            System.out.println("Element type = " + element.getClass().getSimpleName()) ;
+            System.out.println("Element toString() = " + element);
+            System.out.println(Arrays.deepToString((Object[]) element));
+        }
     }
 }
